69b9bb806c04a1cc5356f4a447cf750d771d4da2
Taigah <taigah.dev@gmail.com>
2021-02-01 14:44:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Frontend Mentor - Profile card component](https://www.frontendmentor.io/challenges/profile-card-component-cfArpWshJ)
+
+![Preview](desktop-preview.jpg "Preview")
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      sans: ['"Kumbh Sans"', 'sans']
+    },
+    colors: {
+      'dark-cyan': 'hsl(185, 75%, 39%)',
+      'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+      'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+      'dark-gray': 'hsl(0, 0%, 59%)',
+      'light-gray': 'hsl(0, 0%, 90%)',
+      'white': 'hsl(0, 0%, 100%)'
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
